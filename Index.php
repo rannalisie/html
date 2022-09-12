22a7bc21adce7c8cb29f8724e0b051e071056e6f
Ranna Lisie <105607854+rannalisie@users.noreply.github.com>
2022-09-12 18:36:11 +0200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,17 @@
+<html>
+
+    <head>
+        <title>Editora Blink!</title>
+	</head>
+	
+	<body>
+	
+	     <?php
+	         for ( $i = 0 ; $i < 10 ; $i++) {
+			     print( "Linha número " . $i . "<br />");
+		    }
+	     ?>
+		 
+	</body>
+	
+</html>
---
+++ accordion.php
@@ -0,0 +1,65 @@
+<html>
+<html lang="pt">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+    <title>Editora Aquileana</title>
+  
+      <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+        
+        <script>
+             $( function() {
+             $( "#accordion" ).accordion();
+              } );
+         </script>
+    </head>
+    <body>
+ 
+        <div id="accordion">
+
+        <h3>Quando surgiu a Editora?</h3>
+         <div>
+         <p>
+          Em 2022.
+         </p>
+        </div>
+
+         <h3>Qual o objetivo?</h3>
+         <div>
+         <p>
+         Publicar livros.
+         </p>
+        </div>
+
+        <h3>Quais generos?</h3>
+        <div>
+         <p>
+        LGBTQIA+ entre outros.
+         </p>
+        <ul>
+         <li>Romance.</li>
+         <li>Melodrama.</li>
+         <li>E muito mais.</li>
+        </ul>
+        </div>
+
+        <h3>Para quem são os livros?</h3>
+        <div>
+          <p>
+         Pra você.
+        </p>
+        <p>
+        E para todas as pessoas que quiserem entrar em mundos maravilhosos e histórias clichês e envolvedoras.
+        </p>
+        </div>
+        </div>
+ 
+ 
+    </body>
+    
+</html>
